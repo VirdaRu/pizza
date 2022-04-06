a34a1191d0cbaedae97d02ecaf0572bed2453c7b
Donut-Monkey <54798165+Donut-Monkey@users.noreply.github.com>
2022-04-06 17:07:31 +0200

    Programma uitgebreid met decorator pattern

---
+++ Hawaii.java
@@ -0,0 +1,20 @@
+public class Hawaii extends ToppingDecorator{
+
+    public Hawaii(Pizza newPizza) {
+        super(newPizza);
+
+    }
+    
+    public String getDescription() {
+
+        return tempPizza.getDescription() + "Ananas \nHam";
+    }
+    public double getCost() {
+
+        return tempPizza.getCost() + 1.00;
+    }
+    public int getToppings() {
+
+        return tempPizza.getToppings() + 2;
+    }
+}
---
+++ Pizza.java
@@ -0,0 +1,6 @@
+//decorator pattern
+public interface Pizza {
+    public String getDescription();
+    public double getCost();
+    public int getToppings();
+}
